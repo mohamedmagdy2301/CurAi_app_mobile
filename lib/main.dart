0cba1e30ab35ae836549482a4c2d875e781a54ad
Mohamed magdy <mohammedmego15@gmail.com>
2024-11-30 00:42:21 +0200

    refactor smartcare app

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:smartcare_app_mobile/core/app/env.variables.dart';
-import 'package:smartcare_app_mobile/core/app/smartcare_app.dart';
 import 'package:smartcare_app_mobile/core/app/value_listenable_builder.dart';
 import 'package:smartcare_app_mobile/core/local_storage/shared_preferences_manager.dart';
 import 'package:smartcare_app_mobile/core/simple_bloc_observer/bolc_observer.dart';
@@ -19,5 +18,8 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(
-      ConnectionInternetListener(environment: EnvVariables.instance.debugMode));
+    ConnectionInternetListener(
+      environment: EnvVariables.instance.debugMode,
+    ),
+  );
 }
