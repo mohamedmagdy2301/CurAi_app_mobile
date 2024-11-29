2716c41b957f3093b5620801484fe9e7ff50cd17
Mohamed magdy <mohammedmego15@gmail.com>
2024-11-30 00:42:02 +0200

    refactor smartcare app

--- lib/core/app/smartcare_app.dart
+++ lib/core/app/smartcare_app.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:smartcare_app_mobile/core/app/connectivity_controller.dart';
 import 'package:smartcare_app_mobile/core/common/functions/build_app_connectivity_controller.dart';
-import 'package:smartcare_app_mobile/core/common/screens/no_internet_connection.dart';
 import 'package:smartcare_app_mobile/core/language/app_localizations_setup.dart';
 import 'package:smartcare_app_mobile/core/routes/app_routes.dart';
 import 'package:smartcare_app_mobile/core/styles/themes/app_theme.dart';
@@ -13,25 +11,18 @@ class SmartCareApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ValueListenableBuilder(
-      valueListenable: ConnectivityController.instance.isInternetNotifier,
-      builder: (_, value, __) {
-        if (value) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: environment,
-            theme: AppTheme.lightTheme,
-            darkTheme: AppTheme.darkTheme,
-            builder: (context, child) => buildAppConnectivityController(child),
-            onGenerateRoute: AppRoutes.onGenerateRoute,
-            home: const TestOne(),
-            locale: const Locale('en'),
-            supportedLocales: AppLocalSetup.supportedLocales,
-            localeResolutionCallback: AppLocalSetup.localeResolutionCallback,
-            localizationsDelegates: AppLocalSetup.localesDelegates,
-          );
-        }
-        return const NoInternetConnection();
-      },
+    return MaterialApp(
+      debugShowCheckedModeBanner: environment,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: 0 == 0 ? ThemeMode.light : ThemeMode.dark,
+      builder: (context, child) => buildAppConnectivityController(child),
+      onGenerateRoute: AppRoutes.onGenerateRoute,
+      home: const TestOne(),
+      locale: const Locale('en'),
+      supportedLocales: AppLocalSetup.supportedLocales,
+      localeResolutionCallback: AppLocalSetup.localeResolutionCallback,
+      localizationsDelegates: AppLocalSetup.localesDelegates,
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,18 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:smartcare_app_mobile/core/app/env.variables.dart';
+import 'package:smartcare_app_mobile/core/app/smartcare_app.dart';
 import 'package:smartcare_app_mobile/core/app/value_listenable_builder.dart';
+import 'package:smartcare_app_mobile/core/local_storage/shared_preferences_manager.dart';
 import 'package:smartcare_app_mobile/core/simple_bloc_observer/bolc_observer.dart';
 import 'package:smartcare_app_mobile/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = SimpleBlocObserver();
+  await Future.wait([
+    SharedPreferencesManager.sharedPreferencesInitialize(),
+  ]);
   await EnvVariables.instance.init(envType: EnvTypeEnum.dev);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
