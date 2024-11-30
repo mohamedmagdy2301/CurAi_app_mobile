8aed28c3fefcaaa212b3684a90bf72ae8227103c
Mohamed magdy <mohammedmego15@gmail.com>
2024-11-30 02:27:56 +0200

    c

--- lib/core/app/value_listenable_builder.dart
+++ lib/core/app/value_listenable_builder.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:smartcare_app_mobile/core/app/connectivity_controller.dart';
-import 'package:smartcare_app_mobile/core/app/smartcare_app.dart';
 import 'package:smartcare_app_mobile/core/common/screens/no_internet_connection.dart';
+import 'package:smartcare_app_mobile/smartcare_app.dart';
 
 class ConnectionInternetListener extends StatelessWidget {
   const ConnectionInternetListener({required this.environment, super.key});
