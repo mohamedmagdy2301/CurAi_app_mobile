a880b01741498d5b377a1cb9d2c3943f62ebf273
Mohamed magdy <mohammedmego15@gmail.com>
2024-12-01 17:53:53 +0200

    add dependency Injection Setup  for ( Controllers, Services, Repositories, Usecases, Cubits )

---
+++ lib/core/di/dependency_injection.dart
@@ -0,0 +1,23 @@
+import 'package:get_it/get_it.dart';
+import 'package:smartcare_app_mobile/core/app/connectivity_controller.dart';
+import 'package:smartcare_app_mobile/core/app/cubit/app_cubit.dart';
+import 'package:smartcare_app_mobile/core/app/env.variables.dart';
+
+GetIt sl = GetIt.instance;
+
+void dependencyInjectionSetup() {
+  sl
+    //! Controllers
+    ..registerLazySingleton(() => ConnectivityController.instance)
+    ..registerLazySingleton(() => EnvVariables.instance)
+
+    //! Services
+
+    //! Repositories
+
+    //! Usecases
+
+    //! Blocs(Cubit)
+    //? App Cubit
+    ..registerFactory<AppCubit>(AppCubit.new);
+}
