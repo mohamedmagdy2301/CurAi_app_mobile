65fa132d878c7ba07073d4b2400e1561359cc49a
mohamedmagdy2301 <mohammedmego15@gmail.com>
2024-12-03 20:15:12 +0200

    add string in loacalization and lang keys

--- lib/core/language/lang_keys.dart
+++ lib/core/language/lang_keys.dart
@@ -1,19 +1,35 @@
 class LangKeys {
   static const String appName = 'app_name';
   static const String skip = 'skip';
-
+//! Onboarding
   static String titleOnboarding1 = 'titleOnboarding1';
   static String titleOnboarding2 = 'titleOnboarding2';
   static String titleOnboarding3 = 'titleOnboarding3';
   static String titleOnboarding4 = 'titleOnboarding4';
-
   static String descraptionOnboarding1 = 'descraptionOnboarding1';
   static String descraptionOnboarding2 = 'descraptionOnboarding2';
   static String descraptionOnboarding3 = 'descraptionOnboarding3';
   static String descraptionOnboarding4 = 'descraptionOnboarding4';
-
   static String next = 'next';
   static String getStarted = 'get_started';
+//! Auth
+  static String welcomBack = 'welcom_back';
+  static String descraptionLogin = 'descraptionLogin';
+  static String email = 'email';
+  static String password = 'password';
+  static String login = 'login';
+  static String forgotPassword = 'forgot_password';
+  static String notHaveAccount = 'not_have_account';
+  static String createRegister = 'create_register';
+  static String descraptionRegister = 'descraptionRegister';
+  static String yourNumber = 'your_number';
+  static String register = 'register';
+  static String alreadyHaveAccount = 'already_have_account';
+  static String orSignInWith = 'or_sign_in_with';
+  static String byLogging = 'by_logging';
+  static String termsOfUse = 'terms_of_use';
+  static String and = 'and';
+  static String privacyPolicy = 'privacy_policy';
 
   static String changeLanguage = 'change_language';
   static String changeTheme = 'change_theme';
