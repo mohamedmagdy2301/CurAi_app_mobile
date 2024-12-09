83d12ddf224907bf6444c3195e4779fdc4236e4c
mohamedmagdy2301 <mohammedmego15@gmail.com>
2024-12-09 18:43:13 +0200

    Done add banner in home

--- lib/features/user/presentation/screens/home_screen.dart
+++ lib/features/user/presentation/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:smartcare_app_mobile/core/helper/functions_helper.dart';
+import 'package:smartcare_app_mobile/features/user/presentation/widgets/banner_home_widget.dart';
 import 'package:smartcare_app_mobile/features/user/presentation/widgets/custom_appbar_home.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -15,11 +16,7 @@ class HomeScreen extends StatelessWidget {
           SliverToBoxAdapter(child: spaceHeight(20)),
           const CustomAppBarHome(),
           SliverToBoxAdapter(child: spaceHeight(20)),
-          const SliverToBoxAdapter(
-            child: Center(
-              child: Text('Home Screen'),
-            ),
-          ),
+          const SliverToBoxAdapter(child: Center(child: BannerHomeWidget())),
         ],
       ),
     );
