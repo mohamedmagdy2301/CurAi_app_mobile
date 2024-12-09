7ced8a981e05d87e7f80af0d6722d67793501309
mohamedmagdy2301 <mohammedmego15@gmail.com>
2024-12-09 05:16:35 +0200

    Done add custom appbar to home screen

--- lib/features/user/presentation/screens/home_screen.dart
+++ lib/features/user/presentation/screens/home_screen.dart
@@ -1,27 +1,27 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:smartcare_app_mobile/core/helper/functions_helper.dart';
+import 'package:smartcare_app_mobile/features/user/presentation/widgets/custom_appbar_home.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        const SliverAppBar(
-          // floating: true,
-          // snap: true,
-          pinned: true,
-          title: Text('Home Screen'),
-          automaticallyImplyLeading: false,
-        ),
-        SliverToBoxAdapter(child: spaceHeight(20)),
-        const SliverToBoxAdapter(
-          child: Center(
-            child: Text('Home Screen'),
+    return SafeArea(
+      bottom: false,
+      child: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(child: spaceHeight(20)),
+          const CustomAppBarHome(),
+          SliverToBoxAdapter(child: spaceHeight(20)),
+          const SliverToBoxAdapter(
+            child: Center(
+              child: Text('Home Screen'),
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
---
+++ lib/features/user/presentation/widgets/custom_appbar_home.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:smartcare_app_mobile/core/extensions/context_extansions.dart';
+import 'package:smartcare_app_mobile/core/language/lang_keys.dart';
+import 'package:smartcare_app_mobile/core/routes/routes.dart';
+import 'package:smartcare_app_mobile/core/styles/fonts/font_weight_helper.dart';
+
+class CustomAppBarHome extends StatefulWidget {
+  const CustomAppBarHome({
+    super.key,
+  });
+
+  @override
+  State<CustomAppBarHome> createState() => _CustomAppBarHomeState();
+}
+
+class _CustomAppBarHomeState extends State<CustomAppBarHome> {
+  int count = 5;
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      // floating: true,
+      // snap: true,
+      backgroundColor: Colors.transparent,
+      automaticallyImplyLeading: false,
+      pinned: true,
+      title: ListTile(
+        title: Text(
+          context.translate(LangKeys.hiMohamed),
+          style: context.textTheme.headlineSmall!.copyWith(
+            fontWeight: FontWeightHelper.bold,
+          ),
+        ),
+        subtitle: Text(
+          context.translate(LangKeys.howAreYouToday),
+          style: context.textTheme.bodyMedium!.copyWith(
+            color: context.colors.textColorLight,
+          ),
+        ),
+        trailing: IconButton(
+          onPressed: () {
+            setState(() {
+              count = 0;
+              context.pushNamed(Routes.notificationScreen);
+            });
+          },
+          iconSize: 28.sp,
+          icon: Badge.count(
+            count: count,
+            isLabelVisible: count != 0,
+            child: Icon(
+              CupertinoIcons.bell,
+              color: context.colors.primaryColor,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
