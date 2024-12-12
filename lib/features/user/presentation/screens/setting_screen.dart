29374dcc8d5c1cc7cba2f65e25ecac798ca1e075
mohamedmagdy2301 <mohammedmego15@gmail.com>
2024-12-12 03:27:27 +0200

    z

--- lib/features/user/presentation/screens/setting_screen.dart
+++ lib/features/user/presentation/screens/setting_screen.dart
@@ -1,15 +1,14 @@
 // ignore_for_file: inference_failure_on_instance_creation
 
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:curai_app_mobile/core/app/cubit/app_cubit.dart';
-import 'package:curai_app_mobile/core/extensions/context_extansions.dart';
 import 'package:curai_app_mobile/core/helper/functions_helper.dart';
 import 'package:curai_app_mobile/core/helper/snackbar_helper.dart';
 import 'package:curai_app_mobile/core/language/app_localizations.dart';
 import 'package:curai_app_mobile/core/language/lang_keys.dart';
-import 'package:curai_app_mobile/core/routes/routes.dart';
-import 'package:curai_app_mobile/features/auth/presentation/screens/register_screen.dart';
+import 'package:curai_app_mobile/features/auth/presentation/screens/login_screen.dart';
+import 'package:expansion_tile_card/expansion_tile_card.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SettingScreen extends StatelessWidget {
   const SettingScreen({super.key});
@@ -34,70 +33,66 @@ class SettingScreen extends StatelessWidget {
             _buildSectionTitle(
               context,
               isArabic() ? 'الاعــدادات العامة' : 'General Settings',
-            ),
-            spaceHeight(10),
-            _buildListTile(
-              context,
-              icon: Icons.language,
-              title: AppLocalizations.of(context)!
-                  .translate(LangKeys.changeLanguage),
-              trailing: Switch(
-                value: isEnglish,
-                onChanged: (value) {
-                  if (value) {
-                    context.read<AppCubit>().toEnglish();
-                  } else {
-                    context.read<AppCubit>().toArabic();
-                  }
-                },
-              ),
-            ),
-            const Divider(thickness: .5),
-            _buildListTile(
-              context,
-              icon: Icons.brightness_6,
-              title:
-                  AppLocalizations.of(context)!.translate(LangKeys.changeTheme),
-              trailing: Switch.adaptive(
-                value: isDarkMode,
-                onChanged: (value) {
-                  context.read<AppCubit>().changeTheme();
-                },
-              ),
-            ),
-            const Divider(thickness: .5),
-            _buildListTile(
-              context,
-              icon: Icons.notifications,
-              title: isArabic() ? 'عـرض الأشعـــارات' : 'Show Notifections',
-              onTap: () {
-                showMessage(
+              children: [
+                spaceHeight(10),
+                _buildListTile(
                   context,
-                  type: SnackBarType.success,
-                  message: isEnglish ? 'Notifecation' : 'اشعــــــار',
-                );
-              },
-            ),
-            const Divider(thickness: .5),
-            _buildListTile(
-              context,
-              title: 'Logout',
-              icon: Icons.logout,
-              onTap: () => context.pushNamed(Routes.loginScreen),
-            ),
-            const Divider(thickness: .5),
-            _buildListTile(
-              context,
-              icon: Icons.login,
-              title: AppLocalizations.of(context)!.translate(LangKeys.login),
-              onTap: () {
-                Navigator.of(context).pushAndRemoveUntil(
-                  MaterialPageRoute(
-                    builder: (context) => const RegisterScreen(),
+                  icon: Icons.language,
+                  title: AppLocalizations.of(context)!
+                      .translate(LangKeys.changeLanguage),
+                  trailing: Switch(
+                    value: isEnglish,
+                    onChanged: (value) {
+                      if (value) {
+                        context.read<AppCubit>().toEnglish();
+                      } else {
+                        context.read<AppCubit>().toArabic();
+                      }
+                    },
                   ),
-                  (route) => false,
-                );
-              },
+                ),
+                const Divider(thickness: .5),
+                _buildListTile(
+                  context,
+                  icon: Icons.brightness_6,
+                  title: AppLocalizations.of(context)!
+                      .translate(LangKeys.changeTheme),
+                  trailing: Switch.adaptive(
+                    value: isDarkMode,
+                    onChanged: (value) {
+                      context.read<AppCubit>().changeTheme();
+                    },
+                  ),
+                ),
+                const Divider(thickness: .5),
+                _buildListTile(
+                  context,
+                  icon: Icons.notifications,
+                  title: isArabic() ? 'عـرض الأشعـــارات' : 'Show Notifections',
+                  onTap: () {
+                    showMessage(
+                      context,
+                      type: SnackBarType.success,
+                      message: isEnglish ? 'Notifecation' : 'اشعــــــار',
+                    );
+                  },
+                ),
+                const Divider(thickness: .5),
+                _buildListTile(
+                  context,
+                  icon: Icons.login,
+                  title:
+                      AppLocalizations.of(context)!.translate(LangKeys.login),
+                  onTap: () {
+                    Navigator.of(context).pushAndRemoveUntil(
+                      MaterialPageRoute(
+                        builder: (context) => const LoginScreen(),
+                      ),
+                      (route) => false,
+                    );
+                  },
+                ),
+              ],
             ),
           ],
         ),
@@ -105,15 +100,25 @@ class SettingScreen extends StatelessWidget {
     );
   }
 
-  Widget _buildSectionTitle(BuildContext context, String? title) {
+  Widget _buildSectionTitle(
+    BuildContext context,
+    String? title, {
+    required List<Widget> children,
+  }) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 12),
-      child: Text(
-        title ?? '',
-        style: Theme.of(context)
-            .textTheme
-            .titleMedium
-            ?.copyWith(fontWeight: FontWeight.bold),
+      child: ExpansionTileCard(
+        baseColor: Theme.of(context).cardColor,
+        expandedColor: Theme.of(context).cardColor,
+        elevation: 0,
+        title: Text(
+          title ?? '',
+          style: Theme.of(context)
+              .textTheme
+              .titleMedium
+              ?.copyWith(fontWeight: FontWeight.bold),
+        ),
+        children: children,
       ),
     );
   }
