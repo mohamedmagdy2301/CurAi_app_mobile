1cf4df6589c5d3a1d08fcc53d2824ec499f81e1e
mohamedmagdy2301 <mohammedmego15@gmail.com>
2024-12-12 16:21:01 +0200

    added images for doctor specialities refactor names in appimages

--- lib/core/styles/images/app_images.dart
+++ lib/core/styles/images/app_images.dart
@@ -1,12 +1,31 @@
 class AppImages {
-  static const String noNetwork = 'assets/images/no-internet-connection.png';
-  static const String underBuild = 'assets/images/page_under_build.jpg';
-  static const String onboardingDoctor1 =
-      'assets/images/onboarding-doctor-1.png';
-  static const String onboardingDoctor2 =
-      'assets/images/onboarding-doctor-2.png';
-  static const String onboardingDoctor3 =
-      'assets/images/onboarding-doctor-3.png';
-  static const String onboardingDoctor4 =
-      'assets/images/onboarding-doctor-4.png';
+  static const String images = 'assets/images';
+  static const String noNetwork = '$images/no-internet-connection.png';
+  static const String underBuild = '$images/page_under_build.jpg';
+  static const String onboardingDoctor1 = '$images/onboarding-doctor-1.png';
+  static const String onboardingDoctor2 = '$images/onboarding-doctor-2.png';
+  static const String onboardingDoctor3 = '$images/onboarding-doctor-3.png';
+  static const String onboardingDoctor4 = '$images/onboarding-doctor-4.png';
+}
+
+class SvgImages {
+  static const String svg = 'assets/svg';
+  static const String google = '$svg/google.svg';
+  static const String facebook = '$svg/facebook.svg';
+  static const String apple = '$svg/apple.svg';
+
+  // ! doctor speciailities
+  static const String doctorSpeciality = '$svg/doctor_speciality';
+  static const String general = '$doctorSpeciality/general.svg';
+  static const String cardiologist = '$doctorSpeciality/cardiologist.svg';
+  static const String dentistry = '$doctorSpeciality/dentistry.svg';
+  static const String ent = '$doctorSpeciality/ent.svg';
+  static const String intestine = '$doctorSpeciality/intestine.svg';
+  static const String histologist = '$doctorSpeciality/histologist.svg';
+  static const String hepatology = '$doctorSpeciality/hepatology.svg';
+  static const String neurologic = '$doctorSpeciality/neurologic.svg';
+  static const String pulmonary = '$doctorSpeciality/pulmonary.svg';
+  static const String optometry = '$doctorSpeciality/optometry.svg';
+  static const String urologist = '$doctorSpeciality/urologist.svg';
+  static const String pediatric = '$doctorSpeciality/pediatric.svg';
 }
