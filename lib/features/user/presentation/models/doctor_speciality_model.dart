79c26fe39eaa268fc1eb1b885f266baf2f1d7306
mohamedmagdy2301 <mohammedmego15@gmail.com>
2024-12-12 17:57:28 +0200

    create model and list for doctor speciailities

---
+++ lib/features/user/presentation/models/doctor_speciality_model.dart
@@ -0,0 +1,10 @@
+class DoctorSpecialityModel {
+  DoctorSpecialityModel({
+    required this.id,
+    required this.name,
+    required this.image,
+  });
+  final int id;
+  final String name;
+  final String image;
+}
