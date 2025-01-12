b85f7b33ecca7ba4a0d67900c7de7de025874540
mohamedmagdy2301 <mohammedmego15@gmail.com>
2025-01-12 13:42:25 +0200

    Refactor DoctorSpecialityItemWidget styles for improved color handling and text styling

--- lib/features/user/presentation/widgets/home/doctor_speciality/doctor_speciality_item_widget.dart
+++ lib/features/user/presentation/widgets/home/doctor_speciality/doctor_speciality_item_widget.dart
@@ -1,4 +1,5 @@
 import 'package:curai_app_mobile/core/extensions/settings_context_extansions.dart';
+import 'package:curai_app_mobile/core/extensions/style_text_context_ext.dart';
 import 'package:curai_app_mobile/core/helper/functions_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -19,7 +20,7 @@ class DoctorSpecialityItemWidget extends StatelessWidget {
     return Column(
       children: [
         CircleAvatar(
-          backgroundColor: context.colors.primary.withAlpha(100),
+          backgroundColor: context.color.onSecondary.withAlpha(30),
           radius: 26.r,
           child: image.contains('.svg')
               ? SvgPicture.asset(
@@ -38,9 +39,7 @@ class DoctorSpecialityItemWidget extends StatelessWidget {
         spaceHeight(15),
         Text(
           context.translate(title),
-          style: context.textTheme.bodySmall!.copyWith(
-              // color: context.colors.bodyTextOnboarding,
-              ),
+          style: context.styleRegular12,
         ),
       ],
     );
