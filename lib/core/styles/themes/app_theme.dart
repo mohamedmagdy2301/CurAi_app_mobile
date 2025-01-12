9e9868aadc999a55c0187fc4e4b9aac47bb28d47
mohamedmagdy2301 <mohammedmego15@gmail.com>
2025-01-12 02:05:28 +0200

    Remove unused AppTheme class and related theme definitions to streamline codebase

--- lib/core/styles/themes/app_theme.dart
+++ lib/core/styles/themes/app_theme.dart
@@ -17,123 +17,116 @@ class AppTheme {
     );
     final extension = themeExtensions[themeMode]![palette];
 
+    // Text Theme
+    TextTheme textTheme() {
+      return TextTheme(
+        bodyLarge:
+            AppTextStyles.bodyLarge(context, color: colorScheme.onSurface),
+        bodyMedium:
+            AppTextStyles.bodyMedium(context, color: colorScheme.onSurface),
+        bodySmall: AppTextStyles.bodySmall(
+          context,
+          color: colorScheme.onSurface.withValues(alpha: 0.7),
+        ),
+        titleLarge:
+            AppTextStyles.titleLarge(context, color: colorScheme.onSurface),
+        titleMedium: AppTextStyles.titleMedium(
+          context,
+          color: colorScheme.onSurface.withValues(alpha: 0.8),
+        ),
+        titleSmall: AppTextStyles.bodySmall(
+          context,
+          color: colorScheme.onSurface.withValues(alpha: 0.6),
+        ),
+      );
+    }
+
+    // AppBar Theme
+    AppBarTheme appBarTheme() {
+      return AppBarTheme(
+        titleTextStyle:
+            AppTextStyles.appBarTitle(context, color: colorScheme.onPrimary),
+        // color: colorScheme,
+        elevation: 0,
+        iconTheme: IconThemeData(color: colorScheme.onSurface),
+      );
+    }
+
+    // Floating Action Button Theme
+    FloatingActionButtonThemeData fabTheme() {
+      return FloatingActionButtonThemeData(
+        backgroundColor: colorScheme.secondary,
+        foregroundColor: colorScheme.onSecondary,
+      );
+    }
+
+    // Input Decoration Theme
+    InputDecorationTheme inputDecorationTheme() {
+      OutlineInputBorder buildBorder(Color color, {double width = 1.0}) {
+        return OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(15.r)),
+          borderSide: BorderSide(color: color, width: width.w),
+        );
+      }
+
+      return InputDecorationTheme(
+        errorBorder: buildBorder(colorScheme.error),
+        focusedErrorBorder: buildBorder(colorScheme.error, width: 2.w),
+        errorStyle: AppTextStyles.bodySmall(context, color: colorScheme.error),
+      );
+    }
+
+    // Card Theme
+    CardTheme cardTheme() {
+      return CardTheme(
+        color: colorScheme.surface,
+        elevation: 2,
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.r)),
+      );
+    }
+
+    // Elevated Button Theme
+    ElevatedButtonThemeData buttonTheme() {
+      return ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          foregroundColor: colorScheme.onSecondary,
+          backgroundColor: colorScheme.secondary,
+          elevation: 0,
+          fixedSize: Size(800.w, 50.h),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.r),
+          ),
+        ),
+      );
+    }
+
+    // Snack Bar Theme
+    SnackBarThemeData snackBarTheme() {
+      return SnackBarThemeData(
+        contentTextStyle: AppTextStyles.bodyLarge(context, color: Colors.white),
+        backgroundColor: colorScheme.primary,
+        behavior: SnackBarBehavior.floating,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.r),
+        ),
+      );
+    }
+
     return ThemeData(
       colorScheme: colorScheme,
       extensions: [extension!],
       primaryColor: colorScheme.primary,
       scaffoldBackgroundColor: colorScheme.surface,
       useMaterial3: true,
-      textTheme: _textTheme(context, colorScheme),
-      appBarTheme: _appBarTheme(context, colorScheme),
-      floatingActionButtonTheme: _fabTheme(colorScheme),
-      inputDecorationTheme: _inputDecorationTheme(context, colorScheme),
-      cardTheme: _cardTheme(colorScheme),
-      elevatedButtonTheme: _buttonTheme(colorScheme),
-      snackBarTheme: _snackBarTheme(context, colorScheme),
+      textTheme: textTheme(),
+      appBarTheme: appBarTheme(),
+      floatingActionButtonTheme: fabTheme(),
+      inputDecorationTheme: inputDecorationTheme(),
+      cardTheme: cardTheme(),
+      elevatedButtonTheme: buttonTheme(),
+      snackBarTheme: snackBarTheme(),
       visualDensity: VisualDensity.adaptivePlatformDensity,
-    );
-  }
-
-  // Text Theme
-  static TextTheme _textTheme(BuildContext context, ColorScheme colorScheme) {
-    return TextTheme(
-      bodyLarge: AppTextStyles.bodyLarge(context, color: colorScheme.onSurface),
-      bodyMedium:
-          AppTextStyles.bodyMedium(context, color: colorScheme.onSurface),
-      bodySmall: AppTextStyles.bodySmall(
-        context,
-        color: colorScheme.onSurface.withValues(alpha: 0.7),
-      ),
-      titleLarge:
-          AppTextStyles.titleLarge(context, color: colorScheme.onSurface),
-      titleMedium: AppTextStyles.titleMedium(
-        context,
-        color: colorScheme.onSurface.withValues(alpha: 0.8),
-      ),
-      titleSmall: AppTextStyles.bodySmall(
-        context,
-        color: colorScheme.onSurface.withValues(alpha: 0.6),
-      ),
-    );
-  }
-
-  // AppBar Theme
-  static AppBarTheme _appBarTheme(
-    BuildContext context,
-    ColorScheme colorScheme,
-  ) {
-    return AppBarTheme(
-      titleTextStyle:
-          AppTextStyles.appBarTitle(context, color: colorScheme.onPrimary),
-      color: colorScheme.primary,
-      elevation: 0,
-      iconTheme: IconThemeData(color: colorScheme.onPrimary),
-    );
-  }
-
-  // Floating Action Button Theme
-  static FloatingActionButtonThemeData _fabTheme(ColorScheme colorScheme) {
-    return FloatingActionButtonThemeData(
-      backgroundColor: colorScheme.secondary,
-      foregroundColor: colorScheme.onSecondary,
-    );
-  }
-
-  // Input Decoration Theme
-  static InputDecorationTheme _inputDecorationTheme(
-    BuildContext context,
-    ColorScheme colorScheme,
-  ) {
-    OutlineInputBorder buildBorder(Color color, {double width = 1.0}) {
-      return OutlineInputBorder(
-        borderRadius: BorderRadius.all(Radius.circular(15.r)),
-        borderSide: BorderSide(color: color, width: width.w),
-      );
-    }
-
-    return InputDecorationTheme(
-      errorBorder: buildBorder(colorScheme.error),
-      focusedErrorBorder: buildBorder(colorScheme.error, width: 2.w),
-      errorStyle: AppTextStyles.bodySmall(context, color: colorScheme.error),
-    );
-  }
-
-  // Card Theme
-  static CardTheme _cardTheme(ColorScheme colorScheme) {
-    return CardTheme(
-      color: colorScheme.surface,
-      elevation: 2,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.r)),
-    );
-  }
-
-  // Elevated Button Theme
-  static ElevatedButtonThemeData _buttonTheme(ColorScheme colorScheme) {
-    return ElevatedButtonThemeData(
-      style: ElevatedButton.styleFrom(
-        foregroundColor: colorScheme.onSecondary,
-        backgroundColor: colorScheme.secondary,
-        elevation: 0,
-        fixedSize: Size(800.w, 50.h),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10.r),
-        ),
-      ),
-    );
-  }
-
-  // Snack Bar Theme
-  static SnackBarThemeData _snackBarTheme(
-    BuildContext context,
-    ColorScheme colorScheme,
-  ) {
-    return SnackBarThemeData(
-      contentTextStyle: AppTextStyles.bodyLarge(context, color: Colors.white),
-      backgroundColor: colorScheme.primary,
-      behavior: SnackBarBehavior.floating,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(8.r),
-      ),
     );
   }
 }
