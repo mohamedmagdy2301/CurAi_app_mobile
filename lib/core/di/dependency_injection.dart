15fc72d47a76b7c788c8e648655f6f813070f589
mohamedmagdy2301 <mohammedmego15@gmail.com>
2025-03-19 05:26:17 +0200

    Enhance dependency injection setup by adding remote data source, authentication repository, and registration use case

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -1,7 +1,12 @@
 import 'package:curai_app_mobile/core/api/app_interceptors.dart';
+import 'package:curai_app_mobile/core/api/dio_consumer.dart';
 import 'package:curai_app_mobile/core/app/connectivity_controller.dart';
 import 'package:curai_app_mobile/core/app/env.variables.dart';
 import 'package:curai_app_mobile/core/local_storage/shared_preferences_manager.dart';
+import 'package:curai_app_mobile/features/auth/data/datasources/remote_data_source.dart';
+import 'package:curai_app_mobile/features/auth/data/repositories/auth_repo_impl.dart';
+import 'package:curai_app_mobile/features/auth/domain/repositories/auth_repo.dart';
+import 'package:curai_app_mobile/features/auth/domain/usecases/register_usecase.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
@@ -12,11 +17,23 @@ void setupInit() {
     ..registerLazySingleton<ConnectivityController>(ConnectivityController.new)
     ..registerLazySingleton<EnvVariables>(EnvVariables.new)
     ..registerLazySingleton<SharedPrefManager>(SharedPrefManager.new)
-
     //! Networking
     ..registerLazySingleton(() => AppIntercepters(client: sl<Dio>()))
     ..registerLazySingleton(
       () => LogInterceptor(requestBody: true, responseBody: true),
     )
-    ..registerLazySingleton<Dio>(Dio.new);
+    ..registerLazySingleton(() => DioConsumer(client: sl<Dio>()))
+    ..registerLazySingleton<Dio>(Dio.new)
+    //! UseCases
+    ..registerLazySingleton<RegisterUsecase>(
+      () => RegisterUsecase(repository: sl<AuthRepo>()),
+    )
+    //! Repository
+    ..registerLazySingleton<AuthRepo>(
+      () => AuthRepoImpl(remoteDataSource: sl<RemoteDataSource>()),
+    )
+    //! DataSources
+    ..registerLazySingleton<RemoteDataSource>(
+      () => RemoteDataSourceImpl(dioConsumer: sl<DioConsumer>()),
+    );
 }
