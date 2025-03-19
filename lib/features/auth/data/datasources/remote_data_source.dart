e714028c458bf065ff82c3da63686a631d722aa2
mohamedmagdy2301 <mohammedmego15@gmail.com>
2025-03-19 05:10:22 +0200

    Implement remote data source for user registration and refactor auth repository to utilize it

--- lib/features/auth/data/datasources/remote_data_source.dart
+++ lib/features/auth/data/datasources/remote_data_source.dart
@@ -1,1 +1,27 @@
+// ignore_for_file: one_member_abstracts, document_ignores
 
+import 'package:curai_app_mobile/core/api/dio_consumer.dart';
+import 'package:curai_app_mobile/core/api/end_points.dart';
+import 'package:curai_app_mobile/features/auth/data/models/register_model/register_request.dart';
+
+abstract class RemoteDataSource {
+  Future<dynamic> register(
+    RegisterRequest registerRequest,
+  );
+}
+
+class RemoteDataSourceImpl implements RemoteDataSource {
+  RemoteDataSourceImpl({required this.dioConsumer});
+  final DioConsumer dioConsumer;
+
+  @override
+  Future<dynamic> register(
+    RegisterRequest registerRequest,
+  ) async {
+    final response = await dioConsumer.post(
+      EndPoints.register,
+      body: registerRequest.toJson(),
+    );
+    return response;
+  }
+}
--- lib/features/auth/data/repositories/auth_repo_impl.dart
+++ lib/features/auth/data/repositories/auth_repo_impl.dart
@@ -1,27 +1,23 @@
 // ignore_for_file: avoid_dynamic_calls, avoid_catches_without_on_clauses
 
 import 'package:curai_app_mobile/core/api/dio_consumer.dart';
-import 'package:curai_app_mobile/core/api/end_points.dart';
 import 'package:curai_app_mobile/core/error/failure.dart';
+import 'package:curai_app_mobile/features/auth/data/datasources/remote_data_source.dart';
 import 'package:curai_app_mobile/features/auth/data/models/register_model/register_request.dart';
 import 'package:curai_app_mobile/features/auth/domain/repositories/auth_repo.dart';
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 
 class AuthRepoImpl extends AuthRepo {
-  AuthRepoImpl(this.dioConsumer);
-  final DioConsumer dioConsumer;
-
+  AuthRepoImpl({required this.remoteDataSource});
+  final RemoteDataSource remoteDataSource;
   @override
   Future<Either<Failure, String>> register(
     RegisterRequest registerRequest,
   ) async {
     try {
-      final response = await dioConsumer.post(
-        EndPoints.register,
-        body: registerRequest.toJson(),
-      );
-      return Right(response['message'].toString());
+      final result = await remoteDataSource.register(registerRequest);
+      return Right(result['message'].toString());
     } on FailureException catch (e) {
       return Left(e.failure);
     } on DioException catch (e) {
