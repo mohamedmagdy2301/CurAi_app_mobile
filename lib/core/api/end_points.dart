354c54396ac9621e992ec67b1c4b0245174e4ff9
mohamedmagdy2301 <mohammedmego15@gmail.com>
2025-03-20 04:20:17 +0200

    Add login functionality with API integration and model classes

--- lib/core/api/end_points.dart
+++ lib/core/api/end_points.dart
@@ -2,4 +2,5 @@ class EndPoints {
   static const String baseUrl = 'https://mostafa3mad.pythonanywhere.com';
   static const String refreshToken = '$baseUrl/api/token/refresh/';
   static const String register = '$baseUrl/api/register/';
+  static const String login = '$baseUrl/api/login/';
 }
