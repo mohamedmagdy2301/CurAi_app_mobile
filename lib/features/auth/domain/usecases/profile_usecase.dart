e240054019538e4b03d06d591e4397daa217cc97
mohamedmagdy2301 <mohammedmego15@gmail.com>
2025-03-27 23:32:02 +0200

    Add ProfileUsecase to handle profile retrieval logic from the repository

---
+++ lib/features/auth/domain/usecases/profile_usecase.dart
@@ -0,0 +1,14 @@
+import 'package:curai_app_mobile/core/usecases/usecase.dart';
+import 'package:curai_app_mobile/features/auth/data/models/profile/profile_model.dart';
+import 'package:curai_app_mobile/features/auth/domain/repositories/auth_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class ProfileUsecase extends UseCase<Either<String, ProfileModel>, NoParams> {
+  ProfileUsecase({required this.repository});
+
+  final AuthRepo repository;
+  @override
+  Future<Either<String, ProfileModel>> call(NoParams params) async {
+    return repository.getProfile();
+  }
+}
