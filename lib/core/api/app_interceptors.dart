49fb2918da13864bbfe00ff602138a1d564e095b
mohamedmagdy2301 <mohammedmego15@gmail.com>
2025-03-28 01:55:00 +0200

    Remove unused token refresh logic from AppInterceptors and clean up imports

--- lib/core/api/app_interceptors.dart
+++ lib/core/api/app_interceptors.dart
@@ -1,9 +1,7 @@
 // ignore_for_file: strict_raw_type, document_ignores, lines_longer_than_80_chars, avoid_dynamic_calls, inference_failure_on_function_invocation, avoid_catches_without_on_clauses
 
-import 'package:curai_app_mobile/core/api/end_points.dart';
 import 'package:curai_app_mobile/core/local_storage/shared_pref_key.dart';
 import 'package:curai_app_mobile/core/local_storage/shared_preferences_manager.dart';
-import 'package:curai_app_mobile/core/utils/helper/logger_helper.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 
@@ -56,30 +54,6 @@ class AppInterceptors extends Interceptor {
     //   log('==========================================================');
     // }
     super.onError(err, handler);
-  }
-
-  Future<bool> _refreshToken(String refreshToken) async {
-    try {
-      final response = await client.post(
-        EndPoints.refreshToken,
-        data: {'refresh': refreshToken},
-      );
-
-      if (response.statusCode == 200 || response.statusCode == 201) {
-        final newAccessToken = response.data['access'];
-        await CacheDataHelper.setData(
-          key: SharedPrefKey.keyAccessToken,
-          value: newAccessToken,
-        );
-
-        return true;
-      }
-    } catch (e) {
-      if (kDebugMode) {
-        LoggerHelper.error('Error while refreshing token: $e');
-      }
-    }
-    return false;
   }
 
   Future<Response<dynamic>> retry(RequestOptions requestOptions) async {
