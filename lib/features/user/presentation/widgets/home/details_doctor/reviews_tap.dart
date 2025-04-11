27aa26ef20628b9e603bcb4a89cce39920e9d8be
mohamedmagdy2301 <mohammedmego15@gmail.com>
2025-04-12 01:06:21 +0200

    Refactor reviews components: remove unused widgets and update imports in reviews_tap.dart

--- lib/features/user/presentation/widgets/home/details_doctor/reviews_tap.dart
+++ lib/features/user/presentation/widgets/home/details_doctor/reviews_tap.dart
@@ -2,9 +2,9 @@ import 'package:curai_app_mobile/core/extensions/int_extensions.dart';
 import 'package:curai_app_mobile/core/extensions/widget_extensions.dart';
 import 'package:curai_app_mobile/core/language/lang_keys.dart';
 import 'package:curai_app_mobile/core/utils/widgets/custom_button.dart';
+import 'package:curai_app_mobile/features/reviews/presentation/widgets/no_reviews_widget.dart';
+import 'package:curai_app_mobile/features/reviews/presentation/widgets/reviews_widget.dart';
 import 'package:curai_app_mobile/features/user/data/models/doctor/doctor_model.dart';
-import 'package:curai_app_mobile/features/user/presentation/widgets/home/details_doctor/no_reviews_widget.dart';
-import 'package:curai_app_mobile/features/user/presentation/widgets/home/details_doctor/reviews_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
