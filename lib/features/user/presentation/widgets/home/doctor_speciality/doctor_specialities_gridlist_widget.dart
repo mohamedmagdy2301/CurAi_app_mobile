cbd69622e4380f7e59f1f17f347009c162872320
mohamedmagdy2301 <mohammedmego15@gmail.com>
2025-04-12 21:35:03 +0200

    Update DoctorSpecialitiesGridList to enhance empty state messages and improve search prompt localization

--- lib/features/user/presentation/widgets/home/doctor_speciality/doctor_specialities_gridlist_widget.dart
+++ lib/features/user/presentation/widgets/home/doctor_speciality/doctor_specialities_gridlist_widget.dart
@@ -1,4 +1,5 @@
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:curai_app_mobile/core/extensions/int_extensions.dart';
 import 'package:curai_app_mobile/core/extensions/localization_context_extansions.dart';
 import 'package:curai_app_mobile/core/extensions/theme_context_extensions.dart';
 import 'package:curai_app_mobile/core/extensions/widget_extensions.dart';
@@ -29,8 +30,9 @@ class DoctorSpecialitiesGridList extends StatelessWidget {
             ),
             maxLines: 1,
           ),
+          10.hSpace,
           AutoSizeText(
-            context.isStateArabic ? 'ابدا بالبحث' : 'Start Searching',
+            context.isStateArabic ? ' ابدأ بالبــحث 🔎' : ' Start Searching 🔎',
             textAlign: TextAlign.center,
             overflow: TextOverflow.ellipsis,
             style: TextStyleApp.bold28().copyWith(
