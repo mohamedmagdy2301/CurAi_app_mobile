6ca7956579e31ab4995e0213ca3c868ad506d30f
mohamedmagdy2301 <mohammedmego15@gmail.com>
2025-04-18 07:55:53 +0200

    fix: Update CustomAppbarBookAppointment to set background color in flexibleSpace

--- lib/features/appointment/presentation/widgets/custom_appbar_book_appointment.dart
+++ lib/features/appointment/presentation/widgets/custom_appbar_book_appointment.dart
@@ -1,5 +1,6 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:curai_app_mobile/core/extensions/localization_context_extansions.dart';
+import 'package:curai_app_mobile/core/extensions/theme_context_extensions.dart';
 import 'package:curai_app_mobile/core/language/lang_keys.dart';
 import 'package:flutter/material.dart';
 
@@ -13,7 +14,7 @@ class CustomAppbarBookAppointment extends StatelessWidget
   Widget build(BuildContext context) {
     return AppBar(
       elevation: 0,
-      flexibleSpace: Container(),
+      flexibleSpace: Container(color: context.backgroundColor),
       title: AutoSizeText(
         context.translate(LangKeys.bookAppointment),
         maxLines: 1,
