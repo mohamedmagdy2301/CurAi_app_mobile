2a3cf77396a3c75a4c2547b209aa44658cc43d06
mohamedmagdy2301 <mohammedmego15@gmail.com>
2025-04-25 03:33:27 +0300

    feat(appointments): add my appointments screen with pending and paid tabs; update localization keys

--- lib/core/language/lang_keys.dart
+++ lib/core/language/lang_keys.dart
@@ -176,4 +176,7 @@ class LangKeys {
   static const String availableTime = 'available_time';
   static const String notAvailableToBook = 'not_available_to_book';
   static const String paymentBook = 'payment_book';
+  static const String myAppointments = 'my_appointments';
+  static const String pending = 'pending';
+  static const String paided = 'paided';
 }
---
+++ lib/features/appointment/presentation/screens/my_appointment_patient_screen.dart
@@ -0,0 +1,78 @@
+// ignore_for_file: lines_longer_than_80_chars, avoid_field_initializers_in_const_classes, document_ignores
+
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:curai_app_mobile/core/extensions/localization_context_extansions.dart';
+import 'package:curai_app_mobile/core/extensions/theme_context_extensions.dart';
+import 'package:curai_app_mobile/core/language/lang_keys.dart';
+import 'package:curai_app_mobile/core/styles/fonts/app_text_style.dart';
+import 'package:curai_app_mobile/features/profile/presentation/widgets/help_center/contact_us_body_listview.dart';
+import 'package:curai_app_mobile/features/profile/presentation/widgets/help_center/faq_body_listview.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class MyAppointmentPatientScreen extends StatefulWidget {
+  const MyAppointmentPatientScreen({super.key});
+
+  @override
+  State<MyAppointmentPatientScreen> createState() =>
+      _MyAppointmentPatientScreenState();
+}
+
+class _MyAppointmentPatientScreenState extends State<MyAppointmentPatientScreen>
+    with TickerProviderStateMixin {
+  late TabController tabController;
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: Container(color: context.backgroundColor),
+        title: AutoSizeText(
+          context.translate(LangKeys.myAppointments),
+          maxLines: 1,
+          style: TextStyleApp.bold22().copyWith(
+            color: context.onPrimaryColor,
+          ),
+        ),
+        bottom: TabBar(
+          labelColor: context.primaryColor,
+          unselectedLabelColor: context.onPrimaryColor,
+          indicatorColor: context.primaryColor,
+          indicatorWeight: 3.w,
+          indicatorSize: TabBarIndicatorSize.tab,
+          labelStyle: TextStyleApp.bold16().copyWith(
+            color: context.primaryColor,
+          ),
+          unselectedLabelStyle: TextStyleApp.regular16().copyWith(
+            color: context.onPrimaryColor,
+          ),
+          dividerColor: context.onSecondaryColor.withAlpha(120),
+          overlayColor: WidgetStateProperty.all(
+            context.primaryColor.withAlpha(25),
+          ),
+          controller: tabController,
+          onTap: (value) {
+            setState(() {});
+            tabController.index = value;
+          },
+          tabs: [
+            Tab(text: context.translate(LangKeys.pending)),
+            Tab(text: context.translate(LangKeys.paided)),
+          ],
+        ),
+        centerTitle: true,
+      ),
+      body: _bodyWidget[tabController.index],
+    );
+  }
+
+  final List<Widget> _bodyWidget = [
+    const FAQBodyListView(),
+    const ContactUsBodyListview(),
+  ];
+}
