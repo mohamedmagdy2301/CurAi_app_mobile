5a57a108aee4a529f64a9fd1fa7e9e1f4b49b455
mohamedmagdy2301 <mohammedmego15@gmail.com>
2025-04-30 08:11:14 +0300

    feat(appointment_doctor): refactor availability list view to use card widget and add dismiss functionality

--- lib/features/appointment_doctor/presentation/widgets/working_time_doctor_card_widget.dart
+++ lib/features/appointment_doctor/presentation/widgets/working_time_doctor_card_widget.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: lines_longer_than_80_chars
 
 import 'package:auto_size_text/auto_size_text.dart';
-import 'package:collection/collection.dart';
 import 'package:curai_app_mobile/core/extensions/int_extensions.dart' as ext;
 import 'package:curai_app_mobile/core/extensions/localization_context_extansions.dart';
 import 'package:curai_app_mobile/core/extensions/string_extensions.dart';
@@ -14,139 +13,107 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class WorkingTimeDoctorAvailabilityListView extends StatelessWidget {
-  const WorkingTimeDoctorAvailabilityListView({
-    required this.workingTimeList,
-    super.key,
-  });
-  final List<WorkingTimeDoctorAvailableModel> workingTimeList;
+class WorkingTimeDoctorCardWidget extends StatelessWidget {
+  const WorkingTimeDoctorCardWidget({required this.items, super.key});
+  final List<WorkingTimeDoctorAvailableModel> items;
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: workingTimeList.length,
-      itemBuilder: (context, index) {
-        final groupedData = groupBy(
-          workingTimeList,
-          (item) => item.id,
-        );
-        final groupedItemsList = groupedData.entries.toList();
-        if (index < groupedItemsList.length) {
-          final groupedItem = groupedItemsList[index];
-          // final id = groupedItem.key;
-          final items = groupedItem.value;
-          return Card(
-            margin: context.padding(horizontal: 8, vertical: 10),
-            elevation: 3,
-            clipBehavior: Clip.antiAlias,
-            color: context.isDark
-                ? Colors.black45
-                : const Color.fromARGB(255, 232, 232, 232),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(8.r),
+    return Card(
+      margin: context.padding(horizontal: 8, vertical: 10),
+      elevation: 3,
+      clipBehavior: Clip.antiAlias,
+      color: context.isDark
+          ? Colors.black45
+          : const Color.fromARGB(255, 232, 232, 232),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8.r),
+      ),
+      child: ListTile(
+        title: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Icon(
+                  CupertinoIcons.calendar_today,
+                  color: context.primaryColor.withAlpha(160),
+                  size: 26.sp,
+                ).paddingTop(4),
+                8.wSpace,
+                Wrap(
+                  children: [
+                    AutoSizeText(
+                      '${context.translate(
+                        items.length == 1 ? LangKeys.day : LangKeys.days,
+                      )} ',
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyleApp.medium18().copyWith(
+                        color: context.onPrimaryColor,
+                      ),
+                    ),
+                    for (int i = 0; i < items.length; i++)
+                      AutoSizeText(
+                        '${items[i].getLocalizedDays(isArabic: context.isStateArabic).join(', ')}'
+                        '${i != items.length - 1 ? "${i != items.length - 2 ? "," : " ${context.translate(LangKeys.and)}"} " : ''}',
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyleApp.medium18().copyWith(
+                          color: context.onPrimaryColor,
+                        ),
+                      ),
+                  ],
+                ).expand(),
+              ],
             ),
-            child: ListTile(
-              title: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Icon(
-                        CupertinoIcons.calendar_today,
-                        color: context.primaryColor.withAlpha(160),
-                        size: 26.sp,
-                      ).paddingTop(4),
-                      8.wSpace,
-                      Wrap(
-                        children: [
-                          if (items.length == 1)
-                            AutoSizeText(
-                              '${context.translate(LangKeys.day)} ',
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                              style: TextStyleApp.medium18().copyWith(
-                                color: context.onPrimaryColor,
-                              ),
-                            ),
-                          if (items.length > 1)
-                            AutoSizeText(
-                              '${context.translate(LangKeys.days)} ',
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                              style: TextStyleApp.medium18().copyWith(
-                                color: context.onPrimaryColor,
-                              ),
-                            ),
-                          for (int i = 0; i < items.length; i++)
-                            AutoSizeText(
-                              '${items[i].getLocalizedDays(isArabic: context.isStateArabic).join(', ')}'
-                              '${i != items.length - 1 ? "${i != items.length - 2 ? "," : " ${context.translate(LangKeys.and)}"} " : ''}',
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                              style: TextStyleApp.medium18().copyWith(
-                                color: context.onPrimaryColor,
-                              ),
-                            ),
-                        ],
-                      ).expand(),
-                    ],
-                  ),
-                  _customDivider(context),
-                ],
+            Divider(
+              height: 28.h,
+              thickness: .2,
+              color: context.onSecondaryColor.withAlpha(120),
+            ),
+          ],
+        ),
+        subtitle: Wrap(
+          children: [
+            AutoSizeText(
+              '${context.translate(LangKeys.workStartTimeFrom)} ',
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyleApp.regular16().copyWith(
+                color: context.onSecondaryColor,
               ),
-              subtitle: Wrap(
-                children: [
-                  AutoSizeText(
-                    '${context.translate(LangKeys.workStartTimeFrom)} ',
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyleApp.regular16().copyWith(
-                      color: context.onSecondaryColor,
-                    ),
-                  ),
-                  AutoSizeText(
-                    '${items.first.availableFrom?.toLocalizedTime(context)} ',
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyleApp.medium16().copyWith(
-                      color: context.primaryColor,
-                    ),
-                  ),
-                  AutoSizeText(
-                    '${context.translate(LangKeys.to)} ',
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyleApp.regular16().copyWith(
-                      color: context.onSecondaryColor,
-                    ),
-                  ),
-                  AutoSizeText(
-                    '${items.first.availableTo?.toLocalizedTime(context)}',
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyleApp.medium16().copyWith(
-                      color: context.primaryColor,
-                    ),
-                  ),
-                ],
-              ).expand(),
             ),
-          );
-        } else {
-          return const SizedBox.shrink();
-        }
-      },
-    ).paddingSymmetric(horizontal: 10);
-  }
-
-  Divider _customDivider(BuildContext context) {
-    return Divider(
-      height: 28.h,
-      thickness: .2,
-      color: context.onSecondaryColor.withAlpha(120),
+            AutoSizeText(
+              '${items.first.availableFrom?.toLocalizedTime(context)} ',
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyleApp.medium16().copyWith(
+                color: context.primaryColor,
+              ),
+            ),
+            AutoSizeText(
+              '${context.translate(LangKeys.to)} ',
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyleApp.regular16().copyWith(
+                color: context.onSecondaryColor,
+              ),
+            ),
+            AutoSizeText(
+              '${items.first.availableTo?.toLocalizedTime(context)}',
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyleApp.medium16().copyWith(
+                color: context.primaryColor,
+              ),
+            ),
+          ],
+        ).expand(),
+      ),
     );
   }
 }
--- lib/features/appointment_doctor/presentation/widgets/working_time_doctor_listview_widget.dart
+++ lib/features/appointment_doctor/presentation/widgets/working_time_doctor_listview_widget.dart
@@ -1,15 +1,10 @@
 // ignore_for_file: lines_longer_than_80_chars
 
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:collection/collection.dart';
-import 'package:curai_app_mobile/core/extensions/int_extensions.dart' as ext;
-import 'package:curai_app_mobile/core/extensions/localization_context_extansions.dart';
-import 'package:curai_app_mobile/core/extensions/string_extensions.dart';
 import 'package:curai_app_mobile/core/extensions/theme_context_extensions.dart';
 import 'package:curai_app_mobile/core/extensions/widget_extensions.dart';
-import 'package:curai_app_mobile/core/language/lang_keys.dart';
-import 'package:curai_app_mobile/core/styles/fonts/app_text_style.dart';
 import 'package:curai_app_mobile/features/appointment_doctor/data/models/working_time_doctor_available/working_time_doctor_available_model.dart';
+import 'package:curai_app_mobile/features/appointment_doctor/presentation/widgets/working_time_doctor_card_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -33,120 +28,34 @@ class WorkingTimeDoctorAvailabilityListView extends StatelessWidget {
         final groupedItemsList = groupedData.entries.toList();
         if (index < groupedItemsList.length) {
           final groupedItem = groupedItemsList[index];
-          // final id = groupedItem.key;
           final items = groupedItem.value;
-          return Card(
-            margin: context.padding(horizontal: 8, vertical: 10),
-            elevation: 3,
-            clipBehavior: Clip.antiAlias,
-            color: context.isDark
-                ? Colors.black45
-                : const Color.fromARGB(255, 232, 232, 232),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(8.r),
-            ),
-            child: ListTile(
-              title: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Icon(
-                        CupertinoIcons.calendar_today,
-                        color: context.primaryColor.withAlpha(160),
-                        size: 26.sp,
-                      ).paddingTop(4),
-                      8.wSpace,
-                      Wrap(
-                        children: [
-                          if (items.length == 1)
-                            AutoSizeText(
-                              '${context.translate(LangKeys.day)} ',
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                              style: TextStyleApp.medium18().copyWith(
-                                color: context.onPrimaryColor,
-                              ),
-                            ),
-                          if (items.length > 1)
-                            AutoSizeText(
-                              '${context.translate(LangKeys.days)} ',
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                              style: TextStyleApp.medium18().copyWith(
-                                color: context.onPrimaryColor,
-                              ),
-                            ),
-                          for (int i = 0; i < items.length; i++)
-                            AutoSizeText(
-                              '${items[i].getLocalizedDays(isArabic: context.isStateArabic).join(', ')}'
-                              '${i != items.length - 1 ? "${i != items.length - 2 ? "," : " ${context.translate(LangKeys.and)}"} " : ''}',
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                              style: TextStyleApp.medium18().copyWith(
-                                color: context.onPrimaryColor,
-                              ),
-                            ),
-                        ],
-                      ).expand(),
-                    ],
-                  ),
-                  _customDivider(context),
-                ],
+          return Dismissible(
+            key: ValueKey(groupedItem.key),
+            direction: DismissDirection.endToStart,
+            background: Card(
+              margin: context.padding(horizontal: 8, vertical: 10),
+              elevation: 3,
+              clipBehavior: Clip.antiAlias,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.r),
               ),
-              subtitle: Wrap(
-                children: [
-                  AutoSizeText(
-                    '${context.translate(LangKeys.workStartTimeFrom)} ',
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyleApp.regular16().copyWith(
-                      color: context.onSecondaryColor,
-                    ),
-                  ),
-                  AutoSizeText(
-                    '${items.first.availableFrom?.toLocalizedTime(context)} ',
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyleApp.medium16().copyWith(
-                      color: context.primaryColor,
-                    ),
-                  ),
-                  AutoSizeText(
-                    '${context.translate(LangKeys.to)} ',
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyleApp.regular16().copyWith(
-                      color: context.onSecondaryColor,
-                    ),
-                  ),
-                  AutoSizeText(
-                    '${items.first.availableTo?.toLocalizedTime(context)}',
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyleApp.medium16().copyWith(
-                      color: context.primaryColor,
-                    ),
-                  ),
-                ],
-              ).expand(),
+              color: Colors.red,
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: Icon(
+                  CupertinoIcons.trash,
+                  color: Colors.white,
+                  size: 30.sp,
+                ).paddingSymmetric(horizontal: 20),
+              ),
             ),
+            onDismissed: (direction) {},
+            child: WorkingTimeDoctorCardWidget(items: items),
           );
         } else {
           return const SizedBox.shrink();
         }
       },
     ).paddingSymmetric(horizontal: 10);
-  }
-
-  Divider _customDivider(BuildContext context) {
-    return Divider(
-      height: 28.h,
-      thickness: .2,
-      color: context.onSecondaryColor.withAlpha(120),
-    );
   }
 }
