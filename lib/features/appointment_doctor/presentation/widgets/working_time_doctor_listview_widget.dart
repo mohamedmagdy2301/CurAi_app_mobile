7f45dfaed0343c8093a504d9035bbabb688ddad3
mohamedmagdy2301 <mohammedmego15@gmail.com>
2025-04-30 08:15:46 +0300

    feat(appointment_doctor): enhance dismissible widget with localized swipe message and improved icon layout

--- lib/features/appointment_doctor/presentation/widgets/working_time_doctor_listview_widget.dart
+++ lib/features/appointment_doctor/presentation/widgets/working_time_doctor_listview_widget.dart
@@ -1,8 +1,10 @@
 // ignore_for_file: lines_longer_than_80_chars
 
 import 'package:collection/collection.dart';
+import 'package:curai_app_mobile/core/extensions/localization_context_extansions.dart';
 import 'package:curai_app_mobile/core/extensions/theme_context_extensions.dart';
 import 'package:curai_app_mobile/core/extensions/widget_extensions.dart';
+import 'package:curai_app_mobile/core/styles/fonts/app_text_style.dart';
 import 'package:curai_app_mobile/features/appointment_doctor/data/models/working_time_doctor_available/working_time_doctor_available_model.dart';
 import 'package:curai_app_mobile/features/appointment_doctor/presentation/widgets/working_time_doctor_card_widget.dart';
 import 'package:flutter/cupertino.dart';
@@ -42,11 +44,22 @@ class WorkingTimeDoctorAvailabilityListView extends StatelessWidget {
               color: Colors.red,
               child: Align(
                 alignment: Alignment.centerRight,
-                child: Icon(
-                  CupertinoIcons.trash,
-                  color: Colors.white,
-                  size: 30.sp,
-                ).paddingSymmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      context.isStateArabic ? 'اسحب للحذف' : 'Swipe to delete',
+                      style: TextStyleApp.regular18().copyWith(
+                        color: Colors.white,
+                      ),
+                    ),
+                    Icon(
+                      CupertinoIcons.trash,
+                      color: Colors.white,
+                      size: 30.sp,
+                    ).paddingSymmetric(horizontal: 10),
+                  ],
+                ),
               ),
             ),
             onDismissed: (direction) {},
