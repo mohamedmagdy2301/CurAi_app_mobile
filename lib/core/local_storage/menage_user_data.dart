ec7a67ba3bea497d3c40c5c6d2f0bdabaafb266b
mohamedmagdy2301 <mohammedmego15@gmail.com>
2025-05-01 22:35:16 +0300

    feat(chatbot): add clearHistoryChatBot function to manage chat message history

--- lib/core/local_storage/menage_user_data.dart
+++ lib/core/local_storage/menage_user_data.dart
@@ -1,6 +1,7 @@
 import 'package:curai_app_mobile/core/local_storage/shared_pref_key.dart';
 import 'package:curai_app_mobile/core/local_storage/shared_preferences_manager.dart';
 import 'package:curai_app_mobile/features/auth/data/models/login/login_model.dart';
+import 'package:curai_app_mobile/features/chatbot/presentation/cubit/chatbot_cubit.dart';
 
 void saveDataUser({required LoginModel data}) {
   CacheDataHelper.setData(
@@ -51,6 +52,7 @@ Future<void> clearUserData() async {
     CacheDataHelper.removeData(key: SharedPrefKey.keyBonusPoints),
     CacheDataHelper.removeData(key: SharedPrefKey.keyProfilePicture),
   ]);
+  clearHistoryChatBot();
 }
 
 /// get the username from Cache Data Local
