7c9970e5a9ac2b5b5e785a3d735c2cbd649e3de0
mohamedmagdy2301 <mohammedmego15@gmail.com>
2025-05-03 13:50:27 +0300

    feat(profile): add governorates mapping and refactor complete profile form

--- lib/features/auth/presentation/screens/cont_complete_profile_screen.dart
+++ lib/features/auth/presentation/screens/cont_complete_profile_screen.dart
@@ -2,14 +2,11 @@ import 'package:curai_app_mobile/core/dependency_injection/service_locator.dart'
 import 'package:curai_app_mobile/core/extensions/int_extensions.dart';
 import 'package:curai_app_mobile/core/extensions/theme_context_extensions.dart';
 import 'package:curai_app_mobile/core/language/lang_keys.dart';
-import 'package:curai_app_mobile/core/styles/fonts/app_text_style.dart';
 import 'package:curai_app_mobile/features/auth/presentation/cubit/auth_cubit.dart';
 import 'package:curai_app_mobile/features/auth/presentation/widgets/complete_profile/form_cont_complete_profile_widget.dart';
 import 'package:curai_app_mobile/features/auth/presentation/widgets/header_auth_widget.dart';
-import 'package:curai_app_mobile/features/auth/presentation/widgets/terms_and_conditions_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ContCompleteProfileScreen extends StatefulWidget {
   const ContCompleteProfileScreen({super.key});
@@ -20,52 +17,6 @@ class ContCompleteProfileScreen extends StatefulWidget {
 }
 
 class _ContCompleteProfileScreenState extends State<ContCompleteProfileScreen> {
-  String userType = 'patient';
-
-  Widget chooseUserType(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: ['patient', 'doctor'].map((String type) {
-        final isSelected = userType == type;
-        return ChoiceChip(
-          checkmarkColor: Colors.white,
-          label: Row(
-            children: [
-              if (isSelected) 0.wSpace else 10.wSpace,
-              Icon(
-                isSelected ? Icons.person_2_outlined : Icons.person,
-                color: isSelected ? Colors.white : context.primaryColor,
-              ),
-              if (isSelected) 10.wSpace else 15.wSpace,
-              Text(
-                type == 'patient' ? 'Patient' : 'Doctor',
-              ),
-              if (isSelected) 0.wSpace else 5.wSpace,
-            ],
-          ),
-          selected: isSelected,
-          selectedColor: context.primaryColor,
-          backgroundColor: context.backgroundColor,
-          elevation: 6,
-          padding:
-              EdgeInsets.symmetric(horizontal: context.W * 0.035, vertical: 15),
-          labelStyle: TextStyleApp.medium20().copyWith(
-            color: isSelected ? Colors.white : context.primaryColor,
-          ),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10.r),
-            side: BorderSide(color: context.primaryColor),
-          ),
-          onSelected: (bool selected) {
-            setState(() {
-              userType = (selected ? type : userType);
-            });
-          },
-        );
-      }).toList(),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return BlocProvider<AuthCubit>(
@@ -85,15 +36,10 @@ class _ContCompleteProfileScreenState extends State<ContCompleteProfileScreen> {
                     title: LangKeys.completeProfileTitle,
                     descraption: LangKeys.completeProfileDescription,
                   ),
-                  30.hSpace,
-                  chooseUserType(context),
-                  if (userType == 'doctor') 10.hSpace else 20.hSpace,
-                  ContCompleteProfileFormWidget(
-                    isUserDoctor: userType == 'doctor',
-                  ),
+                  const ContCompleteProfileFormWidget(),
                   35.hSpace,
-                  const TermsOfServiceWidget(),
-                  15.hSpace,
+                  // const TermsOfServiceWidget(),
+                  // 15.hSpace,
                 ],
               ),
             ),
--- lib/features/auth/presentation/widgets/complete_profile/form_cont_complete_profile_widget.dart
+++ lib/features/auth/presentation/widgets/complete_profile/form_cont_complete_profile_widget.dart
@@ -1,10 +1,8 @@
 import 'package:curai_app_mobile/core/extensions/int_extensions.dart';
 import 'package:curai_app_mobile/core/extensions/localization_context_extansions.dart';
 import 'package:curai_app_mobile/core/extensions/navigation_context_extansions.dart';
-import 'package:curai_app_mobile/core/extensions/theme_context_extensions.dart';
 import 'package:curai_app_mobile/core/language/lang_keys.dart';
 import 'package:curai_app_mobile/core/routes/routes.dart';
-import 'package:curai_app_mobile/core/styles/fonts/app_text_style.dart';
 import 'package:curai_app_mobile/core/utils/helper/funcations_helper.dart';
 import 'package:curai_app_mobile/core/utils/widgets/adaptive_dialogs/adaptive_dialogs.dart';
 import 'package:curai_app_mobile/core/utils/widgets/custom_button.dart';
@@ -15,11 +13,9 @@ import 'package:curai_app_mobile/features/auth/presentation/cubit/auth_cubit.dar
 import 'package:curai_app_mobile/features/auth/presentation/widgets/height_valid_notifier_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ContCompleteProfileFormWidget extends StatefulWidget {
-  const ContCompleteProfileFormWidget({required this.isUserDoctor, super.key});
-  final bool isUserDoctor;
+  const ContCompleteProfileFormWidget({super.key});
   @override
   State<ContCompleteProfileFormWidget> createState() =>
       _ContCompleteProfileFormWidgetState();
@@ -29,29 +25,18 @@ class _ContCompleteProfileFormWidgetState
     extends State<ContCompleteProfileFormWidget> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-  final TextEditingController _consultationPriceController =
-      TextEditingController();
-
-  final TextEditingController _addressController = TextEditingController();
-  final TextEditingController _bioController = TextEditingController();
-
+  final TextEditingController _noteController = TextEditingController();
+  final TextEditingController _streetController = TextEditingController();
+  final TextEditingController _areaController = TextEditingController();
+  final TextEditingController _cityController = TextEditingController();
+  final TextEditingController _governorateController = TextEditingController();
+  final TextEditingController _countryController = TextEditingController();
+  double latitude = 0;
+  double longitude = 0;
   // A ValueNotifier to track the form validation status
   final ValueNotifier<bool> _isFormValidNotifier = ValueNotifier<bool>(true);
 
-  int? selectedSpecialization;
-
-  void _validateForDoctorForm() {
-    final isValid = _formKey.currentState?.validate() ?? false;
-    _specializationErrorText = selectedSpecialization == null
-        ? context.isStateArabic
-            ? 'من فضلك اختر التخصص'
-            : 'Please select specialization'
-        : null;
-    setState(() {});
-    _isFormValidNotifier.value = isValid && _specializationErrorText == null;
-  }
-
-  void _validateForPatientForm() {
+  void _validateForm() {
     final isValid = _formKey.currentState?.validate() ?? false;
 
     _isFormValidNotifier.value = isValid;
@@ -59,30 +44,22 @@ class _ContCompleteProfileFormWidgetState
 
   void _onContCompletePressed(BuildContext context) {
     hideKeyboard();
-    widget.isUserDoctor ? _validateForDoctorForm() : _validateForPatientForm();
-
+    _validateForm();
     if (_isFormValidNotifier.value) {
       _formKey.currentState?.save();
 
-      final profileRequest = widget.isUserDoctor
-          ? ProfileRequest(
-              role: 'doctor',
-              consultationPrice: _consultationPriceController.text,
-              specialization: selectedSpecialization,
-              bio: _bioController.text,
-            )
-          : ProfileRequest(
-              role: 'patient',
-              location: _addressController.text,
-              bio: _bioController.text,
-            );
+      final profileRequest = ProfileRequest(
+        location: '${_countryController.text} ${_governorateController.text} '
+            '${_cityController.text} ${_areaController.text} '
+            '${_streetController.text} ${_noteController.text}',
+        latitude: latitude,
+        longitude: longitude,
+      );
       context
           .read<AuthCubit>()
           .editProfile(context, profileRequest: profileRequest);
     }
   }
-
-  String? _specializationErrorText;
 
   @override
   Widget build(BuildContext context) {
@@ -91,127 +68,25 @@ class _ContCompleteProfileFormWidgetState
       autovalidateMode: AutovalidateMode.onUserInteraction,
       child: Column(
         children: [
-          if (!widget.isUserDoctor)
-            HeightValidNotifier(isFormValidNotifier: _isFormValidNotifier),
-          if (!widget.isUserDoctor)
-            CustomTextFeild(
-              labelText: context.translate(LangKeys.address),
-              keyboardType: TextInputType.streetAddress,
-              controller: _addressController,
-              onChanged: (_) => widget.isUserDoctor
-                  ? _validateForDoctorForm()
-                  : _validateForPatientForm(),
-            ),
-          if (widget.isUserDoctor)
-            HeightValidNotifier(isFormValidNotifier: _isFormValidNotifier),
-          if (widget.isUserDoctor)
-            CustomTextFeild(
-              labelText: context.translate(LangKeys.consultationPrice),
-              keyboardType: TextInputType.number,
-              controller: _consultationPriceController,
-              onChanged: (_) => widget.isUserDoctor
-                  ? _validateForDoctorForm()
-                  : _validateForPatientForm(),
-            ),
-          if (widget.isUserDoctor)
-            HeightValidNotifier(isFormValidNotifier: _isFormValidNotifier),
-          if (widget.isUserDoctor)
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 16.w,
-                    vertical: 10.h,
-                  ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8.r),
-                    border: Border.all(
-                      color: _specializationErrorText != null
-                          ? Colors.redAccent
-                          : context.primaryColor.withAlpha(100),
-                    ),
-                  ),
-                  child: DropdownButtonHideUnderline(
-                    child: DropdownButton<int>(
-                      isExpanded: true,
-                      borderRadius: BorderRadius.circular(8.r),
-                      elevation: 0,
-                      style: TextStyleApp.regular16().copyWith(
-                        color: context.onPrimaryColor,
-                      ),
-                      icon: Icon(
-                        Icons.keyboard_arrow_down,
-                        size: 30.sp,
-                        color: context.primaryColor,
-                      ),
-                      value: selectedSpecialization,
-                      hint: Text(
-                        context.translate(LangKeys.medicalSpecialization),
-                        style: TextStyleApp.regular16().copyWith(
-                          color: context.primaryColor,
-                        ),
-                      ),
-                      items: [
-                        DropdownMenuItem(
-                          value: 1,
-                          child: Text(
-                            'Audiologist',
-                            style: TextStyleApp.regular16().copyWith(
-                              color: context.onPrimaryColor,
-                            ),
-                          ),
-                        ),
-                        DropdownMenuItem(
-                          value: 2,
-                          child: Text(
-                            'Allergist',
-                            style: TextStyleApp.regular16().copyWith(
-                              color: context.onPrimaryColor,
-                            ),
-                          ),
-                        ),
-                        DropdownMenuItem(
-                          value: 3,
-                          child: Text(
-                            'Andrologists',
-                            style: TextStyleApp.regular16().copyWith(
-                              color: context.onPrimaryColor,
-                            ),
-                          ),
-                        ),
-                      ],
-                      onChanged: (int? newValue) {
-                        setState(() {
-                          selectedSpecialization = newValue;
-                          _specializationErrorText = null;
-                        });
-                      },
-                    ),
-                  ),
-                ),
-                if (_specializationErrorText != null)
-                  Padding(
-                    padding: EdgeInsets.only(top: 5.h, left: 8.w),
-                    child: Text(
-                      _specializationErrorText!,
-                      style: TextStyleApp.regular12().copyWith(
-                        color: Colors.redAccent,
-                      ),
-                    ),
-                  ),
-              ],
-            ),
+          CustomTextFeild(
+            labelText: context.translate(LangKeys.address),
+            keyboardType: TextInputType.streetAddress,
+            controller: _countryController,
+            onChanged: (_) => _validateForm(),
+          ),
           HeightValidNotifier(isFormValidNotifier: _isFormValidNotifier),
           CustomTextFeild(
-            labelText: context.translate(LangKeys.bio),
+            labelText: context.translate(LangKeys.address),
+            keyboardType: TextInputType.streetAddress,
+            controller: _governorateController,
+            onChanged: (_) => _validateForm(),
+          ),
+          HeightValidNotifier(isFormValidNotifier: _isFormValidNotifier),
+          CustomTextFeild(
+            labelText: context.translate(LangKeys.address),
             keyboardType: TextInputType.text,
-            controller: _bioController,
-            maxLines: 3,
-            isLable: false,
-            onChanged: (_) => widget.isUserDoctor
-                ? _validateForDoctorForm()
-                : _validateForPatientForm(),
+            controller: _cityController,
+            onChanged: (_) => _validateForm(),
           ),
           HeightValidNotifier(isFormValidNotifier: _isFormValidNotifier),
           10.hSpace,
@@ -262,9 +137,9 @@ class _ContCompleteProfileFormWidgetState
 
   @override
   void dispose() {
-    _addressController.dispose();
-    _bioController.dispose();
-    _consultationPriceController.dispose();
+    _governorateController.dispose();
+    _cityController.dispose();
+    _streetController.dispose();
     _isFormValidNotifier.dispose();
     super.dispose();
   }
