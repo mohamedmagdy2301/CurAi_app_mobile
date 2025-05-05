b94eb9ec2f13bed3282214049d0e1d1231040d31
mohamedmagdy2301 <mohammedmego15@gmail.com>
2025-05-05 05:17:01 +0300

    feat(appbar): add back navigation button to appointment and doctor detail appbars

--- lib/features/appointment_patient/presentation/widgets/payment_appointment/custom_appbar_payment_appointment.dart
+++ lib/features/appointment_patient/presentation/widgets/payment_appointment/custom_appbar_payment_appointment.dart
@@ -1,7 +1,9 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:curai_app_mobile/core/extensions/localization_context_extansions.dart';
+import 'package:curai_app_mobile/core/extensions/navigation_context_extansions.dart';
 import 'package:curai_app_mobile/core/extensions/theme_context_extensions.dart';
 import 'package:curai_app_mobile/core/language/lang_keys.dart';
+import 'package:curai_app_mobile/core/routes/routes.dart';
 import 'package:flutter/material.dart';
 
 class CustomAppbarPaymentAppointment extends StatelessWidget
@@ -20,6 +22,11 @@ class CustomAppbarPaymentAppointment extends StatelessWidget
         maxLines: 1,
       ),
       centerTitle: true,
+      leading: IconButton(
+        icon: const Icon(Icons.arrow_back_ios),
+        onPressed: () =>
+            context.pushNamedAndRemoveUntil(Routes.mainScaffoldUser),
+      ),
     );
   }
 
