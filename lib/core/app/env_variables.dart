08a39c7e5d57f13614d53fa8362c787e2023c2e1
mohamedmagdy2301 <mohammedmego15@gmail.com>
2025-05-29 22:18:56 +0300

    Refactor environment initialization to streamline loading logic and improve readability

--- lib/core/app/env_variables.dart
+++ lib/core/app/env_variables.dart
@@ -16,26 +16,36 @@ class AppEnvironment {
   String _dsnSentry = '';
   String _orsApiKey = '';
 
-  Future<void> _loadEnvFile() async {
-    if (kDebugMode) {
-      await dotenv.load(fileName: '.env.dev');
-    } else {
-      await dotenv.load(fileName: '.env.prod');
-    }
-  }
-
   Future<void> initializeEnvironment() async {
-    await _loadEnvFile();
+    if (!kIsWeb) {
+      if (kDebugMode) {
+        await dotenv.load(fileName: '.env.dev');
+      } else {
+        await dotenv.load(fileName: '.env.prod');
+      }
 
-    _envType = dotenv.get('ENV_TYPE');
-    _baseApiUrl = dotenv.get('BASE_API_URL');
-    _apiKeyPaymob = dotenv.get('API_KEY_PAYMOB');
-    _cardPaymentMethodIntegrationId =
-        dotenv.get('CARD_PAYMENT_METHOD_INTEGRATION_ID');
-    _walletPaymentMethodIntegrationId =
-        dotenv.get('WALLET_PAYMENT_METHOD_INTEGRATION_ID');
-    _dsnSentry = dotenv.get('DSN_SENTRY');
-    _orsApiKey = dotenv.get('ORS_API_KEY');
+      _envType = dotenv.get('ENV_TYPE');
+      _baseApiUrl = dotenv.get('BASE_API_URL');
+      _apiKeyPaymob = dotenv.get('API_KEY_PAYMOB');
+      _cardPaymentMethodIntegrationId =
+          dotenv.get('CARD_PAYMENT_METHOD_INTEGRATION_ID');
+      _walletPaymentMethodIntegrationId =
+          dotenv.get('WALLET_PAYMENT_METHOD_INTEGRATION_ID');
+      _dsnSentry = dotenv.get('DSN_SENTRY');
+      _orsApiKey = dotenv.get('ORS_API_KEY');
+    } else {
+      _envType = const String.fromEnvironment('ENV_TYPE', defaultValue: 'prod');
+      _baseApiUrl = const String.fromEnvironment('BASE_API_URL');
+      _apiKeyPaymob = const String.fromEnvironment('API_KEY_PAYMOB');
+      _cardPaymentMethodIntegrationId = const String.fromEnvironment(
+        'CARD_PAYMENT_METHOD_INTEGRATION_ID',
+      );
+      _walletPaymentMethodIntegrationId = const String.fromEnvironment(
+        'WALLET_PAYMENT_METHOD_INTEGRATION_ID',
+      );
+      _dsnSentry = const String.fromEnvironment('DSN_SENTRY');
+      _orsApiKey = const String.fromEnvironment('ORS_API_KEY');
+    }
   }
 
   String get baseApiUrl => _baseApiUrl;
