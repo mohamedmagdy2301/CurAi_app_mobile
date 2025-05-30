44f90d6c72606ed3dc38fdacf4f07310761a0390
mohamedmagdy2301 <mohammedmego15@gmail.com>
2025-05-30 17:29:54 +0300

    Refactor HomeLocalDataSource to implement caching for top doctors and update box names for clarity

--- lib/features/home/data/datasources/home_local_data_source.dart
+++ lib/features/home/data/datasources/home_local_data_source.dart
@@ -5,27 +5,51 @@ abstract class HomeLocalDataSource {
   Future<void> cachePopularDoctors(List<DoctorInfoModel> doctors);
   List<DoctorInfoModel> getCachedPopularDoctors();
   void clearPopularDoctorsCache() {}
+
+  Future<void> cacheTopDoctors(List<DoctorInfoModel> doctors);
+  List<DoctorInfoModel> getCachedTopDoctors();
+  void clearTopDoctorsCache() {}
 }
 
 class HomeLocalDataSourceImpl implements HomeLocalDataSource {
-  static const String boxName = 'doctorsBox';
+  static const String popularDoctorsBox = 'popularDoctorsBox';
+  static const String topDoctorsBoxName = 'topDoctorsBox';
 
   @override
   Future<void> cachePopularDoctors(List<DoctorInfoModel> doctors) async {
-    final box = await Hive.openBox<DoctorInfoModel>(boxName);
+    final box = await Hive.openBox<DoctorInfoModel>(popularDoctorsBox);
     await box.clear();
     await box.addAll(doctors);
   }
 
   @override
   List<DoctorInfoModel> getCachedPopularDoctors() {
-    final box = Hive.box<DoctorInfoModel>(boxName);
+    final box = Hive.box<DoctorInfoModel>(popularDoctorsBox);
     return box.values.toList();
   }
 
   @override
   Future<void> clearPopularDoctorsCache() async {
-    final box = await Hive.openBox<DoctorInfoModel>(boxName);
+    final box = await Hive.openBox<DoctorInfoModel>(popularDoctorsBox);
+    await box.clear();
+  }
+
+  @override
+  Future<void> cacheTopDoctors(List<DoctorInfoModel> doctors) async {
+    final box = await Hive.openBox<DoctorInfoModel>(topDoctorsBoxName);
+    await box.clear();
+    await box.addAll(doctors);
+  }
+
+  @override
+  List<DoctorInfoModel> getCachedTopDoctors() {
+    final box = Hive.box<DoctorInfoModel>(topDoctorsBoxName);
+    return box.values.toList();
+  }
+
+  @override
+  Future<void> clearTopDoctorsCache() async {
+    final box = await Hive.openBox<DoctorInfoModel>(topDoctorsBoxName);
     await box.clear();
   }
 }
