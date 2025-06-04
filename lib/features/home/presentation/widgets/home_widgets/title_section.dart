92684030b500e6b0033de82dec38034df6ec7169
mohamedmagdy2301 <mohammedmego15@gmail.com>
2025-06-04 19:18:32 +0300

    feat: Refactor home screen layout and update title section widget for improved styling and functionality

--- lib/features/home/presentation/widgets/home_widgets/title_section.dart
+++ lib/features/home/presentation/widgets/home_widgets/title_section.dart
@@ -3,6 +3,7 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:curai_app_mobile/core/extensions/localization_context_extansions.dart';
 import 'package:curai_app_mobile/core/extensions/theme_context_extensions.dart';
+import 'package:curai_app_mobile/core/extensions/widget_extensions.dart';
 import 'package:curai_app_mobile/core/language/lang_keys.dart';
 import 'package:curai_app_mobile/core/styles/fonts/app_text_style.dart';
 import 'package:flutter/material.dart';
@@ -13,30 +14,28 @@ class TitleSectionWidget extends StatelessWidget {
   final void Function()? onPressed;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: context.padding(horizontal: 20),
-      child: Row(
-        children: [
-          AutoSizeText(
-            title,
-            maxLines: 1,
-            style: TextStyleApp.bold20().copyWith(
+    return Row(
+      children: [
+        AutoSizeText(
+          title,
+          maxLines: 1,
+          style: TextStyleApp.bold20().copyWith(
+            color: context.primaryColor,
+          ),
+        ),
+        const Spacer(),
+        TextButton(
+          style: TextButton.styleFrom(padding: EdgeInsets.zero),
+          onPressed: onPressed,
+          child: AutoSizeText(
+            context.translate(LangKeys.seeAll),
+            style: TextStyleApp.medium12().copyWith(
               color: context.primaryColor,
             ),
+            maxLines: 1,
           ),
-          const Spacer(),
-          TextButton(
-            onPressed: onPressed,
-            child: AutoSizeText(
-              context.translate(LangKeys.seeAll),
-              style: TextStyleApp.bold14().copyWith(
-                color: context.primaryColor,
-              ),
-              maxLines: 1,
-            ),
-          ),
-        ],
-      ),
-    );
+        ),
+      ],
+    ).paddingSymmetric(horizontal: 15);
   }
 }
